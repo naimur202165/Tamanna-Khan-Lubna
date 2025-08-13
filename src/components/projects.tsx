92ba3d13ab3c5768b314522348c4d6a6bd7b76ba
naimur202165 <naimur202165@gmail.com>
2025-08-13 21:25:59 +0600

    resume added

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,23 +1,22 @@
 // Projects.tsx
-'use client';
-import React from "react";
-import SectionHeading from "./section-heading";
+"use client";
 import { projectsData } from "@/lib/data";
-import Project from "./project-card";
 import { useSectionInView } from "@/lib/useInView";
 import AnimatedButton from "./AnimatedButton";
+import Project from "./project-card";
+import SectionHeading from "./section-heading";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.1);
   // Filter projects by category
-  const reels = projectsData.filter(project => project.category === "Reel");
-  const longVideos = projectsData.filter(project => project.category === "Long Video");
+  const reels = projectsData.filter((project) => project.category === "Reel");
+  const longVideos = projectsData.filter(
+    (project) => project.category === "Long Video"
+  );
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>
-        My Reels
-      </SectionHeading>
+      <SectionHeading>My Reels</SectionHeading>
       <div className="flex flex-row flex-wrap justify-center gap-8 mb-28">
         {reels.map((project, index) => (
           <Project
@@ -29,9 +28,7 @@ export default function Projects() {
           />
         ))}
       </div>
-      <SectionHeading>
-        My Long Videos
-      </SectionHeading>
+      <SectionHeading>My Long Videos</SectionHeading>
       <div className="flex flex-row flex-wrap justify-center gap-8 mb-16">
         {longVideos.map((project, index) => (
           <Project
@@ -44,7 +41,7 @@ export default function Projects() {
         ))}
       </div>
       <div className="flex justify-center">
-        <AnimatedButton link="https://drive.google.com/drive/folders/1KxkK_8jVa7JT8bYSia_QriflMjjwWlzX?usp=sharing" />
+        <AnimatedButton link="https://drive.google.com/file/d/1rk7TDYRwgJMqnzOOvVWuMvaK2RizOXoi/view?usp=sharing" />
       </div>
     </section>
   );
